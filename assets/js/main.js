e1381cb9a42b1fb89c3de3b9657da5f4eb50ac22
Hüseyin Aydın <100160834+huseyinaydinn@users.noreply.github.com>
2023-02-04 23:50:10 +0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+/*=============== SHOW HIDDEN - PASSWORD ===============*/
+
+function showHiddenPass(inputPass, inputIcon) {
+  const input = document.getElementById(inputPass);
+  const iconEye = document.getElementById(inputIcon);
+
+  iconEye.addEventListener('click', () => {
+    if (input.type === 'password') {
+      input.type = 'text';
+
+      iconEye.classList.add('ri-eye-line');
+      iconEye.classList.remove('ri-eye-off-line');
+    } else {
+      input.type = 'password';
+      iconEye.classList.add('ri-eye-off-line');
+      iconEye.classList.remove('ri-eye-line');
+    }
+  });
+}
+
+showHiddenPass('input-pass', 'input-icon');
